0a7e7e8fa290ad2506a14214cdc3f202182d7510
starkhasan <alihasan226@gmail.com>
2019-10-02 10:16:27 +0530

    Class File

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,25 +1,12 @@
 import java.util.Scanner;
-import java.io.*;
 class demo
 {
     public static void main(String [] args)
     {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Length of Array : ");
-        int number=sc.nextInt();
-        int arr[]=new int[number];
-        System.out.println("Enter Element in Array : ");
-        for(int i=0;i<number;i++)
+        int a[]=new int[]{1,2,3,4,5,6,7,8,9,10};
+        for(int aa:a)
         {
-            arr[i]=sc.nextInt();
-        }
-
-
-        System.out.println("Array Element : ");
-        for(int i=0;i<number;i++)
-        {
-            System.out.print(arr[i]+" ");
+            System.out.print(aa+" ");
         }
     }
 }
-
