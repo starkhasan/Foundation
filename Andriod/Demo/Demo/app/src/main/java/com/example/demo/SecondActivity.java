2ca17cd5c73d6574ef2b3a3f1ff224d30216f020
starkhasan <alihasan226@gmail.com>
2020-01-01 06:03:30 +0530

    Add Python File

---
+++ Andriod/Demo/Demo/app/src/main/java/com/example/demo/SecondActivity.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+}
---
+++ Andriod/Demo/RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/RecyclerAdapter.java
@@ -0,0 +1,4 @@
+package com.example.recyclerviewdemo;
+
+class RecyclerAdapter {
+}
