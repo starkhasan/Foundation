b697277588b8fb3df0ded416c94a645237d0aebf
starkhasan <alihasan226@gmail.com>
2020-11-13 23:54:41 +0530

    Add HackerCode

--- Data_Structure/Demo.java
+++ Data_Structure/Demo.java
@@ -1,60 +1,21 @@
-//Program for leap year
 import java.util.*;
-import java.io.*;
-class Node{
-    int data;
-    Node next;
-    Node(int data){
-        this.data = data;
-        next = null;
-    }
-}
-public class Demo{
-    static Node insert(Node head,int data){
-        if(head==null)
-            head = new Node(data);
-        else{
-            Node current = head;
-            while(current.next!=null){
-                current = current.next;
-            }
-            current.next = new Node(data);
+class Demo{
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<5;i++)
+            list.add(i);
+        System.out.println(list);
+
+        Integer[] ar = new Integer[list.size()];
+        ar = list.toArray(ar);
+        for (Integer integer : ar) {
+            System.out.println(integer);
         }
-        return head;
-    }
-    static void print(Node head){
-        Node temp = head;
-        while(temp!=null){
-            System.out.print(temp.data+" ");
-            temp = temp.next;
-        }
-    }
-    static Node insertAtLast(Node head,int data){
-        Node temp = head;
-        Node newNode = new Node(data);
-        while(temp.next!=null){
-            temp = temp.next;
-        }
-        temp.next = newNode;
-        return head;
-    }
-    public static void main(String[] args) throws IOException{
-        Node head = null;
-        Scanner sc = new Scanner(System.in);
-        BufferedReader buffer = new BufferedReader(new FileReader("InputDemo.txt"));
-        String input = "";
-        while((buffer.readLine())!=null){
-            input = buffer.readLine();
-        }       
-        for(int i=0;i<input.split(" ").length;i++){
-            head = insert(head,Integer.parseInt(input.split(" ")[i]));
-        }
-        print(head);
-        System.out.println("\nEnter Data");
-        int temp = sc.nextInt();
-        head = insertAtLast(head,temp);
-        print(head);
-        buffer.close();
-        sc.close();
+
+        int[] newAr = list.stream().mapToInt(i -> i).toArray();
+        for(int j=0;j<newAr.length;j++)
+            System.out.print(newAr[j]+" ");
+        
+        System.out.println(newList);
     }
 }
