c1c19b856e714b2867c5b3d4240ad08d6b65c368
starkhasan <alihasan226@gmail.com>
2020-11-18 15:35:48 +0530

    Add NewFoler

--- Data_Structure/Demo.java
+++ Data_Structure/Demo.java
@@ -1,33 +1,7 @@
 import java.util.*;
 import java.io.*;
-class Demo{
-
-    static int alternatingCharacters(String s) {
-        List<String> listString = new ArrayList<String>();
-        String temp = "";
-        for(int i=0;i<s.length();i++){
-            if(i==0){
-                temp = String.valueOf(s.charAt(i));
-                listString.add(temp);
-            }else if(!temp.equals(String.valueOf(s.charAt(i)))){
-                temp = String.valueOf(s.charAt(i));
-                listString.add(temp);
-            }
-        }  
-        if(listString.size() == 0){
-            return 0;
-        }else{
-            return (s.length() - listString.size());
-        }
-    }
+public class Demo{
     public static void main(String[] args) throws IOException{
         BufferedReader buffer = new BufferedReader(new FileReader("InputDemo.txt"));
-        String input = "";
-        while((buffer.readLine())!=null){
-            input = buffer.readLine();
-            System.out.println(alternatingCharacters(input));
-        }
-        buffer.close();
     }
-
 }
