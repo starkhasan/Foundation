dc18bae95ea80756f5ca7ee09b826b4310dfc228
starkhasan <alihasan226@gmail.com>
2021-04-27 11:16:53 +0530

    Add ProblemSolve Example in Dart

--- Dart/demo.dart
+++ Dart/demo.dart
@@ -1,15 +1,38 @@
-class Student {
-  void testFirst() {
-    print("First Test Method");
-  }
+// void main() {
+//   var map = Map<int, String>();
+//   var _listItems = List.generate(10, (index) => index + 1);
+//   var n = _listItems.length;
+//   for (var i = 0; i < _listItems.length; i++) {
+//     map[n - 1 - i] = 'India' + i.toString();
+//   }
 
-  void testSecond() {
-    print("Second Text Method");
-  }
-}
+//   var key = map.keys.toList()..sort();
+//   for (var item in key) {
+//     print(item);
+//   }
+// }
 
 void main() {
-  Student()
-    ..testFirst()
-    ..testSecond();
+  RegExp regExp = RegExp(r"^[0-9]+(\.[0-9]{1,2})?$");
+  var _listDecimalNumber = [
+    12.43,
+    34.54,
+    41.43,
+    45.00,
+    45.123,
+    92929.12,
+    123.12,
+    3.1,
+    76,
+    23.3421,
+    10.112
+  ];
+
+  var _totalSum = 0.0;
+  for (var item in _listDecimalNumber) {
+    if(regExp.hasMatch(item.toString()))
+      _totalSum += item;
+  }
+
+  print('Total Summmation is ${_totalSum.toStringAsFixed(2)}');
 }
