15825843fc2c22efdb97f79bdaa7b88744f347f1
starkhasan <alihasan226@gmail.com>
2021-10-05 11:07:31 +0530

    Add Json Deserialization and Returning HashMap from json

---
+++ Dart/JSON Conversion/ReturingHashMapFromJson.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+class CurrencyValue {
+  int symbolsReturned;
+  String base;
+  Map data;//Returning HashMap
+
+  CurrencyValue(this.symbolsReturned, this.base, this.data);
+
+  CurrencyValue.fromJson(Map<String, dynamic> json)
+      : symbolsReturned = json['symbols_returned'],
+        base = json['base'],
+        data = json['data'];
+}
+
+void main() {
+  var data = '''{
+  "symbols_returned": 147,
+  "base": "USD",
+  "data": {
+    "AED": 3.672940,
+    "AFN": 75.617000,
+    "ALL": 109.780000,
+    "AMD": 490.740000,
+    "ANG": 1.797350,
+    "AOA": 313.147500,
+    "ARS": 37.864500,
+    "AUD": 1.408450,
+    "AWG": 1.800000,
+    "AZN": 1.702500,
+    "BAM": 1.727050,
+    "BBD": 2.004250
+  }
+}''';
+
+  var response = CurrencyValue.fromJson(jsonDecode(data));
+  var dataResponse = response.data;
+  dataResponse.forEach((key, value) {
+    print('$key -> $value');
+  });
+}
