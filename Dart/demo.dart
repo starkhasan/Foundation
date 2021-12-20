8a2d4d8eaf185c94c9c490aab1ea3932386031d9
starkhasan <alihasan226@gmail.com>
2021-12-20 20:34:13 +0530

    Refactor Code

--- Dart/demo.dart
+++ Dart/demo.dart
@@ -1,38 +1,25 @@
-import 'dart:io';
-import 'dart:convert';
-
-class AllState {
-  int id;
-  String name;
-  int countryId;
-  String countryCode;
-  String stateCode;
-  String? type;
-  String? lat;
-  String? lng;
-
-  AllState(this.id, this.name, this.countryId, this.countryCode, this.stateCode,
-      this.type, this.lat, this.lng);
-
-  AllState.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        name = json['name'],
-        countryId = json['country_id'],
-        countryCode = json['country_code'],
-        stateCode = json['state_code'],
-        type = json['type'],
-        lat = json['latitude'],
-        lng = json['longitude'];
+List<int> sortListNumber(List<int> data) {
+  //we will use the insertion sort algorithm for this
+  var key = 0;
+  for (var j = 1; j < data.length; j++) {
+    key = data[j];
+    var i = j - 1;
+    while (i >= 0 && key < data[i]) {
+      data[i + 1] = data[i];
+      i -= 1;
+    }
+    data[i + 1] = key;
+  }
+  return data;
 }
 
-main(List<String> args) async {
-  var file = File('JsonInput.json');
-  if (await file.exists()) {
-    var data = await file.readAsString();
-    var response = List<AllState>.from(
-        jsonDecode(data).map((item) => AllState.fromJson(item)));
-    for (var item in response) {
-      print('Id -> ${item.id}\nName -> ${item.name}\nCountryId ->${item.countryId}\nCountryCode -> ${item.countryCode}\nStateCode -> ${item.stateCode}\nLatitude ${item.lat}\nLongitude -> ${item.lng}\n');
-    }
-  }
+void main(List<String> args) {
+  var listNumber = [2, 3, 4, 5, 6, 1, 8, 7, 9];
+  print('Before Sorting Element');
+  listNumber.forEach((element) {
+    print(element);
+  });
+  print('After Sorting List');
+  listNumber = sortListNumber(listNumber);
+  listNumber.forEach((element) => print(element));
 }
