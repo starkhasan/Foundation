d1b7b123bd5388df53310d9ac5f352dc52587245
starkhasan <alihasan226@gmail.com>
2022-01-08 02:27:41 +0530

    Add SplashScreen and App ICON

--- Andriod/NotesTodo/app/src/main/java/com/traversal/notestodo/view/NotesHomeActivity.kt
+++ Andriod/NotesTodo/app/src/main/java/com/traversal/notestodo/view/NotesHomeActivity.kt
@@ -1,7 +1,16 @@
 package com.traversal.notestodo.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.traversal.notestodo.databinding.ActivityNotesHomeBinding
 
 class NotesHomeActivity : AppCompatActivity(){
 
+    private lateinit var binding: ActivityNotesHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNotesHomeBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+    }
 }
--- Andriod/NotesTodo/app/src/main/java/com/traversal/notestodo/view/SplashActivity.kt
+++ Andriod/NotesTodo/app/src/main/java/com/traversal/notestodo/view/SplashActivity.kt
@@ -1,12 +1,32 @@
 package com.traversal.notestodo.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.traversal.notestodo.R
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import com.traversal.notestodo.databinding.ActivityMainBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var splashLayout: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        splashLayout = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(splashLayout.root)
+
+        val matrices = applicationContext.resources.displayMetrics
+        val width = matrices.widthPixels
+        val height = matrices.heightPixels
+
+        splashLayout.ivSplashImage.layoutParams.height = height / 4
+        splashLayout.ivSplashImage.layoutParams.width = width / 4
+
+        Handler(Looper.getMainLooper()).postDelayed({
+           startActivity(Intent(this,NotesHomeActivity::class.java))
+        },2000)
+
     }
 }
