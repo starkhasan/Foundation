c814dbf50d1e7f52f55fc7af0420a15a72dd6aab
starkhasan <alihasan226@gmail.com>
2022-01-08 01:33:51 +0530

    Add Style to FullScreen and HideActionbar

---
+++ Andriod/NotesTodo/app/src/main/java/com/traversal/notestodo/view/NotesHomeActivity.kt
@@ -0,0 +1,7 @@
+package com.traversal.notestodo.view
+
+import androidx.appcompat.app.AppCompatActivity
+
+class NotesHomeActivity : AppCompatActivity(){
+
+}
--- Andriod/NotesTodo/app/src/main/java/com/traversal/notestodo/view/SplashActivity.kt
+++ Andriod/NotesTodo/app/src/main/java/com/traversal/notestodo/view/SplashActivity.kt
@@ -1,9 +1,10 @@
-package com.traversal.notestodo
+package com.traversal.notestodo.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.traversal.notestodo.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
