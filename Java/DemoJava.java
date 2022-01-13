f66f328294ed1f00a43d90628b5a0eb1f1b67367
starkhasan <alihasan226@gmail.com>
2022-01-13 12:45:20 +0530

    Refactor Code

---
+++ Java/DemoJava.java
@@ -0,0 +1,11 @@
+/**
+ * Program for sum of two number
+ * without using any variable
+ */
+public class DemoJava{
+    public static void main(String[] args) {
+        //we are going to use args of main method
+        System.out.println("Sum = "+(Integer.parseInt(args[0])+Integer.parseInt(args[1])));
+    }
+}
+
