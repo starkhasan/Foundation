fde04a5c0b22d5245db101088de33f76bd9cfb0c
starkhasan <alihasan226@gmail.com>
2022-01-24 21:30:35 +0530

    Fixed Issue of the RandomNumber Game

--- Python/RandomNumberGame.py
+++ Python/RandomNumberGame.py
@@ -5,6 +5,8 @@ class ValueTooLargeError(Error):
     pass
 class ValueTooSmallError(Error):
     pass
+class NotInRangeError(Error):
+    pass
 def Game():
     chance = 2
     i = first = 0
@@ -15,24 +17,27 @@ def Game():
             i=i+1
             print("You have ",chance-i+1," Chance ")
             user_number = int(input("Guess a Number Between {0} - {1} = ".format(first,last)))
-            if user_number < original_number:
+            if user_number < first or user_number > last:
+                raise NotInRangeError
+            elif user_number < original_number:
                 raise ValueTooSmallError
             elif user_number > original_number:
                 raise ValueTooLargeError
             break
+        except NotInRangeError:
+            print('Number not in range\n')
+            i = i - 1
         except ValueTooSmallError:
             print("This value was too small, try again!")
             first = user_number + 1
             print()
         except ValueTooLargeError:
-            print("This value was too large, try again!")
+            print("This value was too large, try again!\n")
             last = user_number - 1
-            print()
         except ValueError:
-            print("Wrong Input Please Enter Number")
+            print("Wrong Input Please Enter Number\n")
             i = i-1
-            print()
-        
+
     if user_number == original_number:
         print("Congratulations! You guessed it correctly.")
         choose_game()
@@ -54,11 +59,11 @@ def choose_game():
             choose_game()
 
 
-user_chioce = input("Do you want to Play Game (Y/N) ")
-if user_chioce=="Y" or user_chioce=="y" or user_chioce=="yes" or user_chioce=="YES" or user_chioce=="Yes" or user_chioce=="ya" or user_chioce=="YA" or user_chioce=="Ya":
+user_chioce = input("Do you want to Play Game (Y/N) ").lower()
+if user_chioce == "y" or user_chioce=="yes" or user_chioce=="ya" :
     print("\nWelcome to Luck by Chance Game\n")
     Game()
-elif user_chioce=="N" or user_chioce=="n" or user_chioce=="NO" or user_chioce=="No" or user_chioce=="no" or user_chioce == "nO":
+elif user_chioce=="n" or user_chioce=="no" :
     print("Thank you have a Nice Day !")
 else:
     print("Wrong Input Please Try Again!!")
