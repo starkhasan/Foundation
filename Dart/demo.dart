d9b968a6d1c63fc12f81f466f770a654165302cd
starkhasan <alihasan226@gmail.com>
2022-05-20 11:42:32 +0530

    Sort List of Model

--- Dart/demo.dart
+++ Dart/demo.dart
@@ -1,69 +1,33 @@
-import 'dart:convert';
+void main(){
+  List<Employee> employeeList = [];
+  employeeList.add(Employee(name: 'Alex', age: 24, salary: 245.0));
+  employeeList.add(Employee(name: 'Martin', age: 39, salary: 120.0));
+  employeeList.add(Employee(name: 'Lara', age: 42, salary: 213.0));
+  employeeList.add(Employee(name: 'Allen', age: 21, salary: 90.0));
+  employeeList.add(Employee(name: 'Johnny', age: 25, salary: 500.0));
+  employeeList.add(Employee(name: 'Tony', age: 66, salary: 112.0));
+  employeeList.add(Employee(name: 'Robert', age: 51, salary: 342.0));
+  employeeList.add(Employee(name: 'MacCorty', age: 49, salary: 411.0));
+  employeeList.add(Employee(name: 'Romonof', age: 37, salary: 492.0));
+  employeeList.add(Employee(name: 'Natasha', age: 33, salary: 531.0));
+  employeeList.add(Employee(name: 'Nebula', age: 19, salary: 871.0));
+  employeeList.add(Employee(name: 'Gamora', age: 45, salary: 102.0));
+  employeeList.add(Employee(name: 'Groot', age: 66, salary: 941.0));
 
-class DataResponse {
-  int albumId;
-  int id;
-  String title;
-  String url;
-  String thumbnailUrl;
+  employeeList.sort(((a, b) => b.salary.compareTo(a.salary)));
 
-  DataResponse(
-      {required this.albumId,
-      required this.id,
-      required this.title,
-      required this.url,
-      required this.thumbnailUrl});
-
-  factory DataResponse.fromJson(Map<String, dynamic> json) {
-    return DataResponse(
-        albumId: json['albumId'],
-        id: json['id'],
-        title: json['title'],
-        url: json['url'],
-        thumbnailUrl: json['thumbnailUrl']);
+  for(var item in employeeList){
+    print('${item.name} => ${item.age} => ${item.salary}');
   }
 }
+class Employee{
+  String name;
+  int age;
+  double salary;
 
-void main() {
-  var data = '''[{
-    "albumId": 1,
-    "id": 1,
-    "title": "accusamus beatae ad facilis cum similique qui sunt",
-    "url": "https://via.placeholder.com/600/92c952",
-    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
-  },
-  {
-    "albumId": 1,
-    "id": 2,
-    "title": "reprehenderit est deserunt velit ipsam",
-    "url": "https://via.placeholder.com/600/771796",
-    "thumbnailUrl": "https://via.placeholder.com/150/771796"
-  },
-  {
-    "albumId": 1,
-    "id": 3,
-    "title": "officia porro iure quia iusto qui ipsa ut modi",
-    "url": "https://via.placeholder.com/600/24f355",
-    "thumbnailUrl": "https://via.placeholder.com/150/24f355"
-  },
-  {
-    "albumId": 1,
-    "id": 4,
-    "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
-    "url": "https://via.placeholder.com/600/d32776",
-    "thumbnailUrl": "https://via.placeholder.com/150/d32776"
-  },
-  {
-    "albumId": 1,
-    "id": 5,
-    "title": "natus nisi omnis corporis facere molestiae rerum in",
-    "url": "https://via.placeholder.com/600/f66b97",
-    "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
-  }]''';
-
-
-  var response = List<DataResponse>.from(jsonDecode(data).map((item) =>DataResponse.fromJson(item)));
-  for(var item in response){
-    print(item.title);
-  }
+  Employee({
+    required this.name,
+    required this.age,
+    required this.salary
+  });
 }
