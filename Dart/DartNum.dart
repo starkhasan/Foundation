5ec4f09e23fb6a7e2c27769bdf142fcc2a9f1c85
starkhasan <alihasan226@gmail.com>
2022-08-14 16:16:42 +0530

    Add Dart Excersie Question

--- Dart/DartNum.dart
+++ Dart/DartNum.dart
@@ -1,10 +1,35 @@
 void main() {
-  num number = 12;
-  print(number);
-  print(number.runtimeType);
-  number += 12.5;
-  print(number.runtimeType);
-  number += 3;
-  print(number.runtimeType);
-  print(number);
+  import 'dart:io';
+  void main() {
+    ///Number data types
+    var a = 10;
+    //checing the type of data type using runtimeType method in dart
+    print(a.runtimeType);
+    ///checking different properties of the int
+    ///isFinite
+    print(a.isFinite);
+    ///isNaN
+    print(a.isNaN);
+    ///isNegative
+    print(a.isNegative);
+    ///isEven
+    print(a.isEven);
+    ///isOdd
+    print(a.isOdd);
+
+
+    ///Checking the different method of the int
+    /// returing the absolute value
+    print(a.abs());
+    ///return the ceil value
+    print(a.ceil());
+    ///return Floor value
+    print(a.floor());
+    ///return integer after discarding fractional digit
+    print(a.truncate());
+    ///return the string equivalent representation of the number
+    print(a.toString().runtimeType);
+
+  }
+
 }
